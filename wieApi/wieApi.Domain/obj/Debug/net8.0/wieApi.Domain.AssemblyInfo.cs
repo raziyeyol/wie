c40500148086d804e34bd5d2c7b3d0aa1289f335
raziyeyolasigmayoglu <raziyeaka89@gmail.com>
2025-11-22 16:18:45 +0000

    Update assembly informational version and runtime identifier paths across multiple projects

--- wieApi/wieApi.Domain/obj/Debug/net8.0/wieApi.Domain.AssemblyInfo.cs
+++ wieApi/wieApi.Domain/obj/Debug/net8.0/wieApi.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("wieApi.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a61a3935c90cfa8e623209b363f05c2645d6c3f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("wieApi.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("wieApi.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
